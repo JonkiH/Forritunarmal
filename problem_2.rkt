921cd9f8687b78959949c0450a2828e1e1b0d2d9
Jón Heiðar Erlendsson <jone11@ru.is>
2015-03-19 16:42:03 +0000

    problem 2 d done

--- problem_2.rkt
+++ problem_2.rkt
@@ -10,24 +10,13 @@
        (cond ((null? lis)'())
              (else (cons (sumList(charactersPerWord(car lis))) (charactersPerSentence(cdr lis)))))))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+(define wordsPerSentence (lambda (lis)
+       (cond ((null? lis)'())
+             (else (cons (length(car lis)) (wordsPerSentence (cdr lis)))))))
 
 
 (define (testA)(sumList ' (1)))
 (define (testB)(charactersPerWord ' (Scheme is so wonderful)))
 (define (testC)(charactersPerSentence '((Hello Mary) (Scheme is so wonderful) (See you later) (John))))
-
-(define (t)(testC))
+(define (testD)(wordsPerSentence '((Hello Mary) (Scheme is so wonderful) (See you later) (John))))
+(define (t)(testD))
